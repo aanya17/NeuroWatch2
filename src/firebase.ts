ee9d360d8823958cea98e20911d78b5540d573a0
aanya17 <154660363+aanya17@users.noreply.github.com>
2026-02-18 00:52:07 +0530

    Create firebase.ts

---
+++ src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "neurowatch-b3b08.firebaseapp.com",
+  databaseURL: "https://neurowatch-b3b08-default-rtdb.firebaseio.com",
+  projectId: "neurowatch-b3b08",
+  storageBucket: "neurowatch-b3b08.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const database = getDatabase(app);
